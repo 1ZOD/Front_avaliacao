c661c50b6cb2818d9fcdbe8290b048035013df3a
1Zod <nathanmarrega@gmail.com>
2023-10-13 19:28:01 -0300

    add_new_habbit_page

---
+++ src/pages/new_habbit/new_habit.tsx
@@ -0,0 +1,24 @@
+import { Title } from "@/components/communs/title";
+import Head from "next/head";
+import Link from "next/link";
+import MyCalendar from "@/components/calendario/calendario";
+
+export default function New_habbit() {
+  return (
+    <>
+      <Head>
+        <title>Daily Habits</title>
+      </Head>
+      <div>
+        <div className="container-title">
+            <Title>
+            <span>Your daily habits</span>
+            </Title>
+        </div>
+        <div className="container-calendario">
+            <MyCalendar/>
+        </div>
+      </div>
+    </>
+  );
+}
